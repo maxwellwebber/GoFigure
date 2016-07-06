e243348083180bbf03c90bfa6dfcaf63aeb81e64
Sean Burt <seandburt@gmail.com>
2016-07-06 19:48:58 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,76 @@
+"use strict";
+
+var express    = require("express");
+var bodyParser = require("body-parser");
+
+//var Storage = require('./lib/MongoDB');
+
+var app = express();
+
+//var db = new Storage(null, null, 'timer');
+
+// use the parse to get JSON objects out of the request. 
+app.use(bodyParser.json());
+
+// server static files from the public/ directory.
+app.use(express.static('client'));
+
+/**
+ * Handle a request for task data.
+ */
+/*app.get("/data", function (req, res) {
+    console.log("GET Request to: /data");
+    
+     db.getAllTasks(function(err, data){
+         if(err){
+            res.status(500).send();
+        }else{
+            res.status(200).json(data);
+        } 
+     });
+    
+});*/
+
+
+/*
+app.post("/add", function (req, res) {
+
+    console.log("POST Request to: /add");
+    
+    db.addTask(req.body, function(err){
+        if(err){
+            res.status(500).send();
+        }else{
+            res.status(200).send();
+        }
+    });
+    
+    res.status(200).send();
+});
+
+app.post("/remove", function (req, res) {
+    
+    console.log("POST Request to: /remove");
+    console.log(req.body);
+
+    db.removeTask(req.body.id, function(err){
+        if(err){
+            res.status(500).send();
+        }else{
+            res.status(200).send();
+        }
+    });
+});
+*/
+
+app.listen(process.env.PORT || 3000, function () {
+    
+    console.log("Listening on port 3000");
+    
+    /*
+    db.connect(function(){
+        // some message here....
+    });
+    */
+    
+});
